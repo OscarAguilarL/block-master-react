cff6a867910ab133491d6e8edb2ed7c35e4cbd1a
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-26 10:09:52 -0500

    add design system defaults

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import { BlockMasterApp } from './BlockMasterApp'
+import './assets/styles/index.css'
 
 ReactDOM.render(
   <React.StrictMode>
