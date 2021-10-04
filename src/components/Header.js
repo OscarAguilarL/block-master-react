1c550216d93a4377291b61ab88ffd7f3247e8d14
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-10-04 16:56:39 -0500

    Add withRouter function to movieSearch component and fix import
    Create useForm custom hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 import '../assets/styles/Navbar.css'
 import logo from '../assets/img/logo-block-master.png'
-import { Search } from './Search'
+import Search from './Search'
 import { useToggle } from '../hooks/useToggle'
 
 export const Header = () => {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import { withRouter } from 'react-router'
 
 import '../assets/styles/Search.css'
+import { useForm } from '../hooks/useForm'
 
-export const Search = () => {
+const Search = ({ history }) => {
+  const [{ searchTerm }, handleInputChange] = useForm({
+    searchTerm: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    history.push(`/search?q=${encodeURI(searchTerm)}`)
+  }
+
   return (
-    <form className="searchForm">
-      <input type="search" placeholder="Busca tu pelicula favorita..." className="search"/>
+    <form className="searchForm" autoComplete="off" onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Busca tu pelicula favorita..."
+        className="search"
+        name="searchTerm"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
       <button type="submit" className="form-button">
         <i className="icon-search"></i>
       </button>
     </form>
   )
 }
+
+export default withRouter(Search)
